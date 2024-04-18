9649ee21edd6fa23ff5ec8fb1695d9d0671ee77b
Jithendra Ganthakoru <jganthakoru@msa.miraclesoft.com>
2024-04-18 15:34:44 +0530

    comminig new canges

--- src/main/java/com/hkdemircan/hello/HelloWorld.java
+++ src/main/java/com/hkdemircan/hello/HelloWorld.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorld {
 
-    @GetMapping("/display")
+    @GetMapping("/")
     public String getMessage(){
         return "Hello World with Docker";
     }
